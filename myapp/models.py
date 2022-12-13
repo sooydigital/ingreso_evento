dc11f7532406aac8c561bfbe08e8c4b835d59c66
yurley <yurs.ksf1@gmail.com>
2022-12-13 01:47:45 -0500

    clean code

---
+++ myapp/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class People(models.Model):
+    full_name = models.CharField(
+        max_length=1024,
+        verbose_name="Nombre completo")
+
+    code = models.CharField(
+        max_length=20,
+        verbose_name="Cedula")
+
+    activated = models.BooleanField(default=False)
+    activated = models.DateTimeField(blank=True, null=True)
+
+
+    def __str__(self):
+        return '{} - {} - {}'.format(self.nombre, self.code, self.activated)
